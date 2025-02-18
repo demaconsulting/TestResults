53d56328a0f52f9afcb76f5434f899618c965518
Malcolm Nixon <Malcolm.nixon@gmail.com>
2025-02-17 22:59:08 -0500

    Add initial project files.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# TestResults
-Dotnet Test Results Library
+# SPDX Model
+
+![GitHub forks](https://img.shields.io/github/forks/demaconsulting/TestResults?style=plastic)
+![GitHub Repo stars](https://img.shields.io/github/stars/demaconsulting/TestResults?style=plastic)
+![GitHub contributors](https://img.shields.io/github/contributors/demaconsulting/TestResults?style=plastic)
+![GitHub](https://img.shields.io/github/license/demaconsulting/TestResults?style=plastic)
+![Build](https://github.com/demaconsulting/SpdxModel/actions/workflows/build_on_push.yaml/badge.svg)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=demaconsulting_TestResults&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=demaconsulting_TestResults)
+[![Security Rating](https://sonarcloud.io/api/project_badges/measure?project=demaconsulting_TestResults&metric=security_rating)](https://sonarcloud.io/summary/new_code?id=demaconsulting_TestResults)
+
+The TestResults library supports saving test results to TRX files.
---
+++ src/DemaConsulting.TestResults/TestCase.cs
@@ -0,0 +1,87 @@
+﻿// Copyright(c) 2025 DEMA Consulting
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace DemaConsulting.TestResults;
+
+/// <summary>
+///     TestCase class
+/// </summary>
+public sealed class TestCase
+{
+    /// <summary>
+    ///     Gets or sets the ID of the test case
+    /// </summary>
+    public Guid TestId { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    ///     Gets or sets the ID of the test execution
+    /// </summary>
+    public Guid ExecutionId { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    ///     Gets or sets the name of the test case
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the test code assembly
+    /// </summary>
+    public string CodeBase { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the name of the class containing the test case
+    /// </summary>
+    public string ClassName { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets the name of the computer that executed the test case
+    /// </summary>
+    public string ComputerName { get; set; } = Environment.MachineName;
+
+    /// <summary>
+    ///     Gets or sets the start time of the test
+    /// </summary>
+    public DateTime StartTime { get; set; } = DateTime.Now;
+
+    /// <summary>
+    ///     Gets or sets the duration of the test case
+    /// </summary>
+    public double Duration { get; set; } = 0;
+
+    /// <summary>
+    ///     Gets or sets the stdout output when running the test case
+    /// </summary>
+    public string SystemOutput { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the stderr output when running the test case
+    /// </summary>
+    public string SystemError { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the outcome of the test case
+    /// </summary>
+    public TestOutcome Outcome { get; set; } = TestOutcome.Skipped;
+
+    /// <summary>
+    ///     Gets or sets the test error information
+    /// </summary>
+    public TestError? Error { get; set; } = null;
+}
